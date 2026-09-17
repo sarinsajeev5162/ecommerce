faea6279d069ca1e3c2de3dc70acd12a195df019
sarinsajeev5162 <sarinsajeev5162@gmail.com>
2026-09-17 11:08:32 +0530

    sample

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,12 @@ ALLOWED_HOSTS = []
 AUTH_USER_MODEL='users.User'
 
 # Application definition
+from datetime import timedelta
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        return user
