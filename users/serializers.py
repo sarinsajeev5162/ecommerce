73b49bbf747b534393da041edbec050019a191d6
sarinsajeev5162 <sarinsajeev5162@gmail.com>
2026-09-18 09:33:25 +0530

    sample

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,25 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'role']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            role=validated_data.get('role', 'customer')
+        )
+        return user
+
+
+# Login Serializer
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
